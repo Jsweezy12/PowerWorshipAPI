5b8291bd0590cecc9d1014c78d7221f57b0385ca
John Swann <john.swann@agapecoders.com>
2020-03-29 16:34:23 -0400

    updated playload size

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import {PowerworshipapiApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
+import {RestBindings} from '@loopback/rest'; // import added (obviously)
+import {PowerworshipapiApplication} from './application';
 
 export {PowerworshipapiApplication};
 
@@ -7,6 +8,7 @@ export async function main(options: ApplicationConfig = {}) {
   const app = new PowerworshipapiApplication(options);
   await app.boot();
   await app.start();
+  app.bind(RestBindings.REQUEST_BODY_PARSER_OPTIONS).to({limit: '50mb'}) // line added
 
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
